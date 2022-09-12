59241b5a8ba067dfebef2a60dbac6316dd53e863
Herald Olivares <heraldmatias.oz@gmail.com>
2022-09-12 03:15:17 +0000

    Adding endpoint to get the data by user

--- realtimeGraph/views.py
+++ realtimeGraph/views.py
@@ -564,6 +564,90 @@ def get_map_json(request, **kwargs):
     return JsonResponse(data_result)
 
 
+def get_user_map_json(request, **kwargs):
+    data_result = {}
+
+    userParam = kwargs.get("user", None)
+    measureParam = kwargs.get("measure", None)
+    selectedMeasure = None
+    measurements = Measurement.objects.all()
+
+    try:
+        user = User.objects.get(login=userParam)
+    except:
+        raise Http404
+
+    if measureParam != None:
+        selectedMeasure = Measurement.objects.filter(name=measureParam)[0]
+    elif measurements.count() > 0:
+        selectedMeasure = measurements[0]
+
+    locations = Location.objects.filter(station__user_id=userParam)
+    try:
+        start = datetime.fromtimestamp(
+            float(request.GET.get("from", None)) / 1000
+        )
+    except:
+        start = None
+    try:
+        end = datetime.fromtimestamp(
+            float(request.GET.get("to", None)) / 1000)
+    except:
+        end = None
+    if start == None and end == None:
+        start = datetime.now()
+        start = start - dateutil.relativedelta.relativedelta(weeks=1)
+        end = datetime.now()
+        end += dateutil.relativedelta.relativedelta(days=1)
+    elif end == None:
+        end = datetime.now()
+    elif start == None:
+        start = datetime.fromtimestamp(0)
+
+    data_result = _get_data_results(
+        locations, user, selectedMeasure, start, end)
+    return JsonResponse(data_result)
+
+
+def _get_data_results(locations, user, selectedMeasure, start, end):
+    data_result = {}
+    data = []
+    start_ts = int(start.timestamp() * 1000000)
+    end_ts = int(end.timestamp() * 1000000)
+
+    for location in locations:
+        stations = Station.objects.filter(location=location)
+        locationData = Data.objects.filter(
+            station__in=stations, measurement__name=selectedMeasure.name, time__gte=start_ts, time__lte=end_ts,
+        )
+        if locationData.count() <= 0:
+            continue
+        minVal = locationData.aggregate(Min("min_value"))["min_value__min"]
+        maxVal = locationData.aggregate(Max("max_value"))["max_value__max"]
+        avgVal = locationData.aggregate(Avg("avg_value"))["avg_value__avg"]
+        data.append(
+            {
+                "location_name": f"{location.city.name}, {location.state.name}, {location.country.name}",
+                "user": f'{user.first_name} {user.last_name}',
+                "lat": location.lat,
+                "lng": location.lng,
+                "population": stations.count(),
+                "min": minVal if minVal != None else 0,
+                "max": maxVal if maxVal != None else 0,
+                "avg": round(avgVal if avgVal != None else 0, 2),
+            }
+        )
+
+    startFormatted = start.strftime("%d/%m/%Y") if start != None else " "
+    endFormatted = end.strftime("%d/%m/%Y") if end != None else " "
+
+    data_result["locations"] = [loc.str() for loc in locations]
+    data_result["start"] = startFormatted
+    data_result["end"] = endFormatted
+    data_result["data"] = data
+    return data_result
+
+
 class RemaView(TemplateView):
     template_name = "rema.html"
 
